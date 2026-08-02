b5e4ee265f9acc940b3aa4e70a742cd41638abbb
Abufauzan1206 <abufauzan1206@gmail.com>
2026-08-02 11:54:15 +0100

    Migrate journal repository to adapter architecture

--- js/repositories/journalRepository.js
+++ js/repositories/journalRepository.js
@@ -4,13 +4,27 @@
  * Repository Module: RP-006
  *
  * File: journalRepository.js
- * Version: 1.0.0
+ * Version: 2.0.0
+ *
+ * Adapter Based Journal Repository
  * =====================================================
  */
 
 import { CMPBaseRepository } from "./baseRepository.js";
+import { CMPAdapterFactory } from "../adapters/adapterFactory.js";
+
 
 export class CMPJournalRepository
     extends CMPBaseRepository {
+
+
+    constructor() {
+
+        super(
+            CMPAdapterFactory.firebase("journals")
+        );
+
+    }
+
 
 }
