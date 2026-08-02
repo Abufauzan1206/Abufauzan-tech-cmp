74d5b33eb4fe3aed5dae054a3e928193f6b61a71
Abufauzan1206 <abufauzan1206@gmail.com>
2026-08-02 12:14:39 +0100

    Migrate contribution repository to adapter architecture

--- js/repositories/contributionRepository.js
+++ js/repositories/contributionRepository.js
@@ -4,13 +4,27 @@
  * Repository Module: RP-003
  *
  * File: contributionRepository.js
- * Version: 1.0.0
+ * Version: 2.0.0
+ *
+ * Adapter Based Contribution Repository
  * =====================================================
  */
 
 import { CMPBaseRepository } from "./baseRepository.js";
+import { CMPAdapterFactory } from "../adapters/adapterFactory.js";
+
 
 export class CMPContributionRepository
     extends CMPBaseRepository {
+
+
+    constructor() {
+
+        super(
+            CMPAdapterFactory.firebase("contributions")
+        );
+
+    }
+
 
 }
