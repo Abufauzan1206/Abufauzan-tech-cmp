ced330bc22f80ec33f3561d70d55d3ec645af33f
Abufauzan1206 <abufauzan1206@gmail.com>
2026-08-02 10:04:19 +0100

    Implement and test Firebase adapter create and findById

---
+++ js/adapters/databaseAdapter.js
@@ -0,0 +1,36 @@
+/**
+ * =====================================================
+ * ABUFAUZAN TECH Cooperative Management Platform
+ * Database Adapter Layer
+ *
+ * File: databaseAdapter.js
+ * Module: DA-001
+ * Version: 1.0.0
+ *
+ * Abstract Database Adapter
+ * =====================================================
+ */
+
+export class CMPDatabaseAdapter {
+
+    async create(data) {
+        throw new Error("create() not implemented");
+    }
+
+    async findById(id) {
+        throw new Error("findById() not implemented");
+    }
+
+    async findAll() {
+        throw new Error("findAll() not implemented");
+    }
+
+    async update(id, data) {
+        throw new Error("update() not implemented");
+    }
+
+    async delete(id) {
+        throw new Error("delete() not implemented");
+    }
+
+}
---
+++ js/adapters/firebaseAdapter.js
@@ -0,0 +1,70 @@
+/**
+ * =====================================================
+ * ABUFAUZAN TECH Cooperative Management Platform
+ * Database Adapter Layer
+ *
+ * File: firebaseAdapter.js
+ * Module: DA-002
+ * Version: 1.3.0
+ *
+ * Firebase Database Adapter
+ * =====================================================
+ */
+
+import { CMPDatabaseAdapter } from "./databaseAdapter.js";
+import { db } from "../firebase-config.js";
+
+import {
+    collection,
+    addDoc,
+    getDoc,
+    getDocs,
+    updateDoc,
+    deleteDoc,
+    doc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+export class CMPFirebaseAdapter extends CMPDatabaseAdapter {
+
+    constructor(collectionName) {
+
+        super();
+
+        this.collectionName = collectionName;
+
+    }
+
+    async create(data) {
+
+        const document = await addDoc(
+            collection(db, this.collectionName),
+            data
+        );
+
+        return document.id;
+
+    }
+
+    async findById(id) {
+
+        const snapshot = await getDoc(
+            doc(db, this.collectionName, id)
+        );
+
+        if (!snapshot.exists()) {
+
+            return null;
+
+        }
+
+        return {
+
+            id: snapshot.id,
+
+            ...snapshot.data()
+
+        };
+
+    }
+
+}
