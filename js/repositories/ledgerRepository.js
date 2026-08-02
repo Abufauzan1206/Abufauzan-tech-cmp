ecbf7eed34035969902721d9daa279f5927b6046
Abufauzan1206 <abufauzan1206@gmail.com>
2026-08-02 12:00:39 +0100

    Migrate ledger repository to adapter architecture

--- js/repositories/ledgerRepository.js
+++ js/repositories/ledgerRepository.js
@@ -4,13 +4,27 @@
  * Repository Module: RP-005
  *
  * File: ledgerRepository.js
- * Version: 1.0.0
+ * Version: 2.0.0
+ *
+ * Adapter Based Ledger Repository
  * =====================================================
  */
 
 import { CMPBaseRepository } from "./baseRepository.js";
+import { CMPAdapterFactory } from "../adapters/adapterFactory.js";
+
 
 export class CMPLedgerRepository
     extends CMPBaseRepository {
+
+
+    constructor() {
+
+        super(
+            CMPAdapterFactory.firebase("ledgers")
+        );
+
+    }
+
 
 }
