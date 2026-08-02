1c9edb670645cf922815761fb046a3a244582aea
Abufauzan1206 <abufauzan1206@gmail.com>
2026-08-02 14:20:03 +0100

    Add journal service integration layer

---
+++ js/services/journalService.js
@@ -0,0 +1,60 @@
+/**
+ * =====================================================
+ * ABUFAUZAN TECH Cooperative Management Platform
+ *
+ * Service Layer
+ *
+ * File: journalService.js
+ * Version: 1.0.0
+ *
+ * Journal Business Service
+ * =====================================================
+ */
+
+
+import { CMPRepositoryManager } 
+from "../repositories/repositoryManager.js";
+
+
+const journalRepository =
+    CMPRepositoryManager.get("journal");
+
+
+
+export async function createJournal(data) {
+
+    return await journalRepository.create(data);
+
+}
+
+
+
+export async function getJournalById(id) {
+
+    return await journalRepository.findById(id);
+
+}
+
+
+
+export async function getAllJournals() {
+
+    return await journalRepository.findAll();
+
+}
+
+
+
+export async function updateJournal(id, data) {
+
+    return await journalRepository.update(id, data);
+
+}
+
+
+
+export async function deleteJournal(id) {
+
+    return await journalRepository.delete(id);
+
+}
